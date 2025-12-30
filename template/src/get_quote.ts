ad49ed40d95e2b381a8e72ec77d3ae7119272d7a
stepan_romankov <stepan@romankov.name>
2025-12-30 16:26:33 +0100

    fix sdk autoupdate

--- template/src/get_quote.ts
+++ template/src/get_quote.ts
@@ -3,14 +3,12 @@ import {fromProtoDecimal, toProtoDecimal} from "./lib";
 
 export default async function getQuote(networkClient: Client<typeof NetworkService>) {
   const eurgbp = await networkClient.getQuote({
-    payInCurrency: "EUR",
-    payInMethod: PaymentMethodType.SEPA,
     payOutCurrency: "GBP",
     payOutMethod: PaymentMethodType.SWIFT,
     quoteType: QuoteType.REALTIME,
     amount: {
       amount: {
-        case: 'payInAmount',
+        case: 'payOutAmount',
         value: toProtoDecimal(10, 0)
       },
     }
@@ -18,7 +16,7 @@ export default async function getQuote(networkClient: Client<typeof NetworkServi
 
   switch (eurgbp.result.case) {
     case 'success':
-      console.log(`EUR/GBP: ${fromProtoDecimal(eurgbp.result.value.rate!)}`)
+      console.log(`USD/GBP: ${fromProtoDecimal(eurgbp.result.value.rate!)}`)
       break;
     case 'failure':
       console.error(eurgbp.result.value.reason)
