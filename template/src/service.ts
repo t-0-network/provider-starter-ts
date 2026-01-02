9bddb67e89bc33e3e3f9a0a28985c243a88fe34a
Anton Efimow <anton@efimow.name>
2026-01-02 14:39:17 +0100

    upd proto to latest

--- template/src/service.ts
+++ template/src/service.ts
@@ -1,69 +1,72 @@
 import {
-  PayoutRequest,
-  PayoutResponse,
-  UpdatePaymentRequest,
-  UpdateLimitRequest,
-  UpdateLimitResponse,
-  AppendLedgerEntriesRequest,
-  AppendLedgerEntriesResponse,
-  UpdatePaymentResponse,
-  HandlerContext, type Client, NetworkService, ApprovePaymentQuoteRequest, ApprovePaymentQuoteResponse,
+    AppendLedgerEntriesRequest,
+    AppendLedgerEntriesResponse,
+    ApprovePaymentQuoteRequest,
+    ApprovePaymentQuoteResponse,
+    type Client,
+    HandlerContext,
+    NetworkService,
+    PayoutRequest,
+    PayoutResponse,
+    UpdateLimitRequest,
+    UpdateLimitResponse,
+    UpdatePaymentRequest,
+    UpdatePaymentResponse,
 } from "@t-0/provider-sdk";
 
 /*
   Please refer to docs, proto definition comments or source code comments to understand purpose of fields
  */
 const CreateProviderService = (networkClient: Client<typeof NetworkService>) => {
-  return {
-    async updatePayment(req: UpdatePaymentRequest, _: HandlerContext) {
-      // TODO: Step 2.1 implement how you handle updates of payment initiated by you
-      console.log(`Received payment update for ${req.paymentId}, payment ${req.result.case}`)
-      return {} as UpdatePaymentResponse
-    },
-
-    async payOut(req: PayoutRequest, _: HandlerContext) {
-      // TODO: Step 2.4 implement how you do payouts (payments initiated by your counterparts)
-      console.log(`Received payout request ${req.payoutId}`)
-
-      //TODO: confirmPayout should be called when you system notifies that payout has been made successfully
-      setInterval(() => {
-        networkClient.confirmPayout({
-          paymentId: req.paymentId,
-          payoutId: req.payoutId,
-        })
-      }, 2000);
-      return {
-        result: {
-          case: "accepted",
-          value: {},
+    return {
+        async updatePayment(req: UpdatePaymentRequest, _: HandlerContext) {
+            // TODO: Step 2.1 implement how you handle updates of payment initiated by you
+            console.log(`Received payment update for ${req.paymentId}, payment ${req.result.case}`)
+            return {} as UpdatePaymentResponse
         },
-      } as PayoutResponse
-    },
 
-    async updateLimit(req: UpdateLimitRequest, _: HandlerContext) {
-      // TODO: optionally implement updates on your limits and limits usage
-      console.log(`Received update of limits with provider ${req.limits[0].counterpartId}`)
-      return {} as UpdateLimitResponse
-    },
+        async payOut(req: PayoutRequest, _: HandlerContext) {
+            // TODO: Step 2.4 implement how you do payouts (payments initiated by your counterparts)
+            console.log(`Received payout request ${req.payoutId}`)
 
-    async appendLedgerEntries(req: AppendLedgerEntriesRequest, _: HandlerContext) {
-      // TODO: optionally implement handling of new ledger transactions and new ledger entries
-      console.log(`Received ledger entries for ${req.transactions} transaction(s)`)
-      return {
-      } as AppendLedgerEntriesResponse
-    },
+            //TODO: confirmPayout should be called when you system notifies that payout has been made successfully
+            setInterval(() => {
+                networkClient.confirmPayout({
+                    paymentId: req.paymentId,
+                    payoutId: req.payoutId,
+                })
+            }, 2000);
+            return {
+                result: {
+                    case: "accepted",
+                    value: {},
+                },
+            } as PayoutResponse
+        },
 
-    async approvePaymentQuote(req: ApprovePaymentQuoteRequest, _: HandlerContext) {
-      // TODO: when the payment goes through the Manual AML Check on the pay-out provider side, the provider submitted the payment will have a last look to approve final quote
-      console.log(`Received approve payment quote request for ${req.paymentId}`)
-      return {
-        result: {
-          case: "accepted",
-          value: {}
+        async updateLimit(req: UpdateLimitRequest, _: HandlerContext) {
+            // TODO: optionally implement updates on your limits and limits usage
+            console.log(`Received update of limits with provider ${req.limits[0].counterpartId}`)
+            return {} as UpdateLimitResponse
+        },
+
+        async appendLedgerEntries(req: AppendLedgerEntriesRequest, _: HandlerContext) {
+            // TODO: optionally implement handling of new ledger transactions and new ledger entries
+            console.log(`Received ledger entries for ${req.transactions} transaction(s)`)
+            return {} as AppendLedgerEntriesResponse
+        },
+
+        async approvePaymentQuote(req: ApprovePaymentQuoteRequest, _: HandlerContext) {
+            // TODO: when the payment goes through the Manual AML Check on the pay-out provider side, the provider submitted the payment will have a last look to approve final quote
+            console.log(`Received approve payment quote request for ${req.paymentId}`)
+            return {
+                result: {
+                    case: "accepted",
+                    value: {}
+                }
+            } as ApprovePaymentQuoteResponse
         }
-      } as ApprovePaymentQuoteResponse
     }
-  }
 };
 
 export default CreateProviderService;
